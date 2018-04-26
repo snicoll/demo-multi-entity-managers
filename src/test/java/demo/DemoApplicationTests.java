c09ca2ec06e41ced6700c5d9a36b79de6f4a550e
Stephane Nicoll <snicoll@pivotal.io>
2018-04-26 15:43:31 +0200

    Add integration test
    Closes gh-2

---
+++ src/test/java/demo/DemoApplicationTests.java
@@ -0,0 +1,89 @@
+package demo;
+
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.Date;
+
+import javax.sql.DataSource;
+
+import demo.customer.domain.Customer;
+import demo.customer.domain.CustomerRepository;
+import demo.order.domain.Order;
+import demo.order.domain.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Autowired
+	@Qualifier("customerDataSource")
+	private DataSource customerDataSource;
+
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	@Qualifier("orderDataSource")
+	private DataSource orderDataSource;
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Test
+	public void customerDataSourceIsInitializedProperly() {
+		JdbcTemplate customerTemplate = new JdbcTemplate(this.customerDataSource);
+		assertThat(hasTable(customerTemplate, "CUSTOMER")).isTrue();
+		assertThat(hasTable(customerTemplate, "CUSTOMER_ORDER")).isFalse();
+	}
+
+	@Test
+	public void customerRepositoryConfiguredProperly() {
+		JdbcTemplate customerTemplate = new JdbcTemplate(this.customerDataSource);
+		int count = countItem(customerTemplate, "CUSTOMER");
+		Customer customer = new Customer("John", "Smith");
+		this.customerRepository.save(customer);
+		assertThat(countItem(customerTemplate, "CUSTOMER")).isEqualTo(++count);
+	}
+
+	@Test
+	public void orderDataSourceIsInitializedProperly() {
+		JdbcTemplate orderTemplate = new JdbcTemplate(this.orderDataSource);
+		assertThat(hasTable(orderTemplate, "CUSTOMER_ORDER")).isTrue();
+		assertThat(hasTable(orderTemplate, "CUSTOMER")).isFalse();
+	}
+
+	@Test
+	public void orderRepositoryConfiguredProperly() {
+		JdbcTemplate orderTemplate = new JdbcTemplate(this.orderDataSource);
+		int count = countItem(orderTemplate, "CUSTOMER_ORDER");
+		Order order = new Order(123L, new Date());
+		this.orderRepository.save(order);
+		assertThat(countItem(orderTemplate, "CUSTOMER_ORDER")).isEqualTo(++count);
+	}
+
+	private boolean hasTable(JdbcTemplate jdbcTemplate, String tableName) {
+		return jdbcTemplate.execute((ConnectionCallback<Boolean>) connection -> {
+			DatabaseMetaData md = connection.getMetaData();
+			try (ResultSet rs = md.getTables(null, null, tableName, new String[] { "TABLE" })) {
+				return (rs.next());
+			}
+		});
+	}
+
+	private int countItem(JdbcTemplate jdbcTemplate, String tableName) {
+		return jdbcTemplate.queryForObject(String.format(
+				"SELECT COUNT(*) from %s", tableName), Integer.class);
+	}
+
+}
